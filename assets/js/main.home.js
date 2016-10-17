372d2f9a4730517aef6b56064a108e846838d9d1
zhoujunpeng <zjp0432@163.com>
2016-10-17 13:49:43 +0800

    3

--- assets/js/main.home.js
+++ assets/js/main.home.js
@@ -167,9 +167,7 @@
     ev.stopPropagation();
     $(this).parents('.page__entry__cover').hide();
     $(this).parents('.page__nav__cover').hide();
-    $(this).parents('.vfx__showcases').hide();
-    $(this).parents('.animation__showcases').hide();
-    $(this).parents('.origin__showcases').hide();
+    $(this).parents('.slider__page__showcases').hide();
     $sliderBtns.show();
     $spotBox.show();
     if (locked) {
